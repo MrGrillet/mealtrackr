5eac0fdfd7a54a0865da73cd6579f15c31d8df9f
Peter <pagrillet@gmail.com>
2018-09-25 00:30:36 +0100

    manually compiling assets

--- config/application.rb
+++ config/application.rb
@@ -13,8 +13,7 @@ module Mealtrackr
 
     # config.serve_static_assets = true
 
-    config.assets.precompile << 'application_body.js'
-  config.assets.initialize_on_precompile = false
+    config.serve_static_assets = true
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
