d69f438bdf6932b42bcf6716978fe075982e5a02
Peter <pagrillet@gmail.com>
2018-09-25 02:21:46 +0100

    still scrppin

--- config/application.rb
+++ config/application.rb
@@ -11,8 +11,9 @@ module Mealtrackr
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
 
-    config.assets.initialize_on_precompile = false
-    
+    config.assets.initialize_on_precompile = true
+
+    Rails.application.config.assets.precompile += %w( scripts.js )
     # config.serve_static_assets = true
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
