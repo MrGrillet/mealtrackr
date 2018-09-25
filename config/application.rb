b4c363a3d6d205c4ddbb870468570244e1bed6f7
Peter <pagrillet@gmail.com>
2018-09-25 12:14:57 +0100

    moving the js to the bottom of the page

--- config/application.rb
+++ config/application.rb
@@ -11,9 +11,9 @@ module Mealtrackr
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
 
-    config.assets.initialize_on_precompile = true
+    config.assets.initialize_on_precompile = false
 
-    Rails.application.config.assets.precompile += %w( scripts.js )
+
     # config.serve_static_assets = true
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
